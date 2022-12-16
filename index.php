4b02c9f35e3418bc918db1efce6555c6bbeb28a4
amel62 <rizqiiameliaa@gmail.com>
2022-12-16 10:00:47 +0700

    rdf dah bisa ges

--- index.php
+++ index.php
@@ -2,7 +2,33 @@
 require 'vendor/autoload.php';
 require_once __DIR__."/html_tag_helpers.php";
 
+    // \EasyRdf\RdfNamespace::setDefault('og');
+
+    \EasyRdf\RdfNamespace::set('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+    \EasyRdf\RdfNamespace::set('rdfs','http://www.w3.org/2000/01/rdf-schema#');
+    \EasyRdf\RdfNamespace::set('dbo', 'http://dbpedia.org/ontology/');
+    \EasyRdf\RdfNamespace::set('dbp', 'http://dbpedia.org/property/');
     \EasyRdf\RdfNamespace::setDefault('og');
+
+$sparql_jena = new \EasyRdf\Sparql\Client('http://localhost:3030/sukarno/sparql');
+
+$sparql_query = 'select ?synopsis ?child1  where{
+  ?m dbo:child1 ?child1;
+     rdfs:synopsis ?synopsis}';
+
+  $result = $sparql_jena->query($sparql_query);
+
+  foreach($result as $row){
+    echo '<br>';
+    echo '<b>';
+    echo $row -> child1;
+    echo '</b>';
+    echo '<br/>';
+    echo '<b>';
+    echo $row -> synopsis;
+    echo '</b>';
+    echo '<br/>';
+    }
 
 ?>
 <!DOCTYPE html>
