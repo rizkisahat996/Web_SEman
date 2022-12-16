ab4c4dc2d86fb098d7b138600f0c782f2230df23
davidbangun31 <stephandavid88@gmail.com>
2022-12-16 08:45:40 +0700

    OGP

--- index.php
+++ index.php
@@ -1,6 +1,53 @@
 <?php
 require 'vendor/autoload.php';
+require_once __DIR__."/html_tag_helpers.php";
 
-echo("hehe");
+    \EasyRdf\RdfNamespace::setDefault('og');
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <title>Document</title> -->
+    <title>Tubes Web Semantik</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <style type="text/css">
+    body { font-family: sans-serif; }
+    dt { font-weight: bold; }
+    .image { float: left; margin: 10px; max-width: 200px}
+  </style>
+</head>
+<body>
+    <!-- OGP -->
+<div class="container">
+
+  <div class="row">
+
+    <div class="col-auto">
+      <?php
+        $doc = \EasyRdf\Graph::newAndLoad('https://dbpedia.org/page/Sukarno');
+        if ($doc->image) {
+          echo content_tag('img', null, array('src'=>$doc->image, 'class'=>'image'));
+        }
+      ?>
+    </div>
+    
+    <div class="col-9">
+      <dl>
+        <dt>Page:</dt> <dd><?= link_to($doc->url) ?></dd>
+        <dt>Type:</dt> <dd><code><?= $doc->type ?></code></dd>
+        <dt>Title:</dt> <dd><?= $doc->title ?></dd>
+        <dt>Description:</dt> <dd><?= $doc->description ?></dd>
+      </dl>
+    </div>
+
+  </div>
+  
+  
+</div>
+
+</body>
+</html>
